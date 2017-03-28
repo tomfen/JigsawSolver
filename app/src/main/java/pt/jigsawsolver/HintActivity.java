d6f26656dd08beeddad220366ea7ad56b57a96c0
Dominika Czarnecka <dominikaczarnecka@MacBook-Air-Dominika.local>
2017-03-28 19:14:41 +0200

    [FEATURES] choose image from gallery
    Added handler for choose and display image from gallery or documents

--- app/src/main/java/pt/jigsawsolver/HintActivity.java
+++ app/src/main/java/pt/jigsawsolver/HintActivity.java
@@ -1,14 +1,91 @@
 package pt.jigsawsolver;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HintActivity extends Activity {
+
+    ImageButton cameraButton;
+    ImageButton galleryButton;
+    ImageButton saveButton;
+
+    ImageView photoView;
+
+    private static int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hint);
 
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        galleryButton = (ImageButton) findViewById(R.id.galleryButton);
+        saveButton = (ImageButton) findViewById(R.id.saveImgButton);
+
+        photoView = (ImageView) findViewById(R.id.photoView);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                Bitmap pictureBitmap= getBitmapFromUri(data.getData());
+
+                Mat picture = new Mat();
+                Utils.bitmapToMat(pictureBitmap, picture);
+
+                photoView.setImageBitmap(pictureBitmap);
+
+            } catch (Exception e){
+                Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
 }
