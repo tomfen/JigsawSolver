e970622ba2d6113ad4b7cb5a9e3883fd0ebc57b4
tomfen <tomfen999@gmail.com>
2017-04-17 21:03:43 +0200

    Poprawione dopasowywanie

--- app/src/main/java/pt/jigsawsolver/JigsawFitter.java
+++ app/src/main/java/pt/jigsawsolver/JigsawFitter.java
@@ -6,7 +6,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.DMatch;
 import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint2f;
@@ -15,18 +14,32 @@ import org.opencv.core.Scalar;
 import org.opencv.features2d.*;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class JigsawFitter {
-    public static Mat find(Mat element, Mat template) {
+
+    private static final int FEATURE_DETECTOR = FeatureDetector.ORB;
+    private static final int DESCRIPTOR_EXTRACTOR = DescriptorExtractor.ORB;
+    private static final int FEATURE_MATCHER = DescriptorMatcher.BRUTEFORCE_HAMMING;
+    private static final double DISTANCE_RATIO = 3.;
+    private static final int MAX_MATCHES = 10;
+    private static final double REPROJECT_ERROR = 8.;
+
+    private static final Scalar OUTLINE_COLOR = new Scalar(0, 255, 0);
+    private static final int OULINE_THICKNESS = 6;
+
+
+    public static Mat find(Mat img_object1, Mat img_scene1) {
         Mat img_object = new Mat();
         Mat img_scene = new Mat();
 
-        Imgproc.cvtColor(element, img_object, Imgproc.COLOR_RGBA2RGB, 1);
-        Imgproc.cvtColor(template, img_scene, Imgproc.COLOR_RGBA2RGB, 1);
+        Imgproc.cvtColor(img_object1, img_object, Imgproc.COLOR_RGBA2RGB, 1);
+        Imgproc.cvtColor(img_scene1, img_scene, Imgproc.COLOR_RGBA2RGB, 1);
 
-        FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB); //SURF i SIFT nie działa
+        FeatureDetector detector = FeatureDetector.create(FEATURE_DETECTOR); //SURF i SIFT nie działa
 
         MatOfKeyPoint keypoints_object = new MatOfKeyPoint();
         MatOfKeyPoint keypoints_scene  = new MatOfKeyPoint();
@@ -34,7 +47,7 @@ public class JigsawFitter {
         detector.detect(img_object, keypoints_object);
         detector.detect(img_scene, keypoints_scene);
 
-        DescriptorExtractor extractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+        DescriptorExtractor extractor = DescriptorExtractor.create(DESCRIPTOR_EXTRACTOR);
 
         Mat descriptor_object = new Mat();
         Mat descriptor_scene = new Mat();
@@ -42,7 +55,7 @@ public class JigsawFitter {
         extractor.compute(img_object, keypoints_object, descriptor_object);
         extractor.compute(img_scene, keypoints_scene, descriptor_scene);
 
-        DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+        DescriptorMatcher matcher = DescriptorMatcher.create(FEATURE_MATCHER);
         MatOfDMatch matches = new MatOfDMatch();
 
         matcher.match(descriptor_object, descriptor_scene, matches);
@@ -57,17 +70,27 @@ public class JigsawFitter {
             if(dist > max_dist) max_dist = dist;
         }
 
-        //System.out.println("-- Max dist : " + max_dist);
-        //System.out.println("-- Min dist : " + min_dist);
+        System.out.println("-- Max dist : " + max_dist);
+        System.out.println("-- Min dist : " + min_dist);
 
         LinkedList<DMatch> good_matches = new LinkedList<DMatch>();
         MatOfDMatch gm = new MatOfDMatch();
 
         for(int i = 0; i < descriptor_object.rows(); i++){
-            if(matchesList.get(i).distance <= 2*min_dist){
+            if(matchesList.get(i).distance <= DISTANCE_RATIO*min_dist){
                 good_matches.addLast(matchesList.get(i));
             }
         }
+        class DistanceComparator implements Comparator<DMatch> {
+            @Override
+            public int compare(DMatch a, DMatch b) {
+                return (int) (a.distance - b.distance);
+            }
+        }
+
+        Collections.sort(good_matches, new DistanceComparator());
+        if (good_matches.size() > MAX_MATCHES)
+            good_matches = new LinkedList<DMatch>(good_matches.subList(0, MAX_MATCHES));
 
         gm.fromList(good_matches);
 
@@ -83,7 +106,7 @@ public class JigsawFitter {
                 Scalar.all(-1),
                 Scalar.all(-1),
                 new MatOfByte(),
-                Features2d.NOT_DRAW_SINGLE_POINTS | Features2d.DRAW_RICH_KEYPOINTS);*/
+                0);*/
 
         LinkedList<Point> objList = new LinkedList<Point>();
         LinkedList<Point> sceneList = new LinkedList<Point>();
@@ -103,7 +126,7 @@ public class JigsawFitter {
         scene.fromList(sceneList);
 
         try {
-            Mat homography = Calib3d.findHomography(obj, scene);
+            Mat homography = Calib3d.findHomography(obj, scene, Calib3d.RANSAC, REPROJECT_ERROR);
 
             Mat obj_corners = new Mat(4,1,CvType.CV_32FC2);
             Mat scene_corners = new Mat(4,1, CvType.CV_32FC2);
@@ -124,17 +147,14 @@ public class JigsawFitter {
                 scene_corners.put(i, 0, pt);
             }*/
 
-            Scalar color = new Scalar(0, 255, 0);
-
-            Imgproc.line(img_scene, new Point(scene_corners.get(0,0)), new Point(scene_corners.get(1,0)), color, 4);
-            Imgproc.line(img_scene, new Point(scene_corners.get(1,0)), new Point(scene_corners.get(2,0)), color, 4);
-            Imgproc.line(img_scene, new Point(scene_corners.get(2,0)), new Point(scene_corners.get(3,0)), color, 4);
-            Imgproc.line(img_scene, new Point(scene_corners.get(3,0)), new Point(scene_corners.get(0,0)), color, 4);
-
-            return img_scene;
+            Imgproc.line(img_scene, new Point(scene_corners.get(0,0)), new Point(scene_corners.get(1,0)), OUTLINE_COLOR, OULINE_THICKNESS);
+            Imgproc.line(img_scene, new Point(scene_corners.get(1,0)), new Point(scene_corners.get(2,0)), OUTLINE_COLOR, OULINE_THICKNESS);
+            Imgproc.line(img_scene, new Point(scene_corners.get(2,0)), new Point(scene_corners.get(3,0)), OUTLINE_COLOR, OULINE_THICKNESS);
+            Imgproc.line(img_scene, new Point(scene_corners.get(3,0)), new Point(scene_corners.get(0,0)), OUTLINE_COLOR, OULINE_THICKNESS);
 
         } catch (Exception e) {
             return null;
         }
+        return img_scene;
     }
 }
